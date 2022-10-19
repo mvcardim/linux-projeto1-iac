90d02d730bf49d9a1b26d3e83df82c0e9545e340
mvcardim <68335567+mvcardim@users.noreply.github.com>
2022-10-19 16:46:21 -0300

    Update criar_user.sh

--- criar_user.sh
+++ criar_user.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-
 echo "Criando usuários do sistema...."
 
+useradd user1 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+passwd user1 -e
 
-for i in $(awk ´{print $1}´ usuario)  
-do
-    useradd guest$i -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
-    grep $i usuarios | awk ´{print $1}´
-done
+useradd user2 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+passwd user2 -e
 
+useradd user3 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+passwd user3 -e
+
+useradd user4 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+passwd user4 -e
 
 echo "Finalizado!!"
-
